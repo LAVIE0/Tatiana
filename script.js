17f2465018d21443952057746db9039f4253a569
Andrewsoares15 <andrewsoares347@gmail.com>
2021-06-30 14:21:12 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
-
 var modal = document.querySelectorAll('a[href^="#"]');
-var div = document.querySelector('.modal-overlay');
-
-function adicionarClasse(){
-    div.classList.add('ativo');
-    }
-modal[0].addEventListener('click', adicionarClasse)
-var div2 = document.querySelector('.modal-overlay1');
-
-function adicionarClasse1(){
-    div2.classList.add('ativo')
+var divs = document.querySelectorAll('.modal-overlay')
+function adicionarClasse(index){
+    divs[index].classList.add('ativo');
 }
-modal[1].addEventListener('click', adicionarClasse1)
 
-var div3 = document.querySelector('.modal-overlay2');
+modal.forEach((itemModal, index)=>{
+    itemModal.addEventListener('click', function(){
+        adicionarClasse(index);
+    })
+})
+var exit = document.querySelectorAll(".exit img");
+function sairModal(index){
+    divs[index].classList.remove('ativo');
+}
+exit.forEach((itemImg, index)=>{
+    itemImg.addEventListener('click', function(){
+        sairModal(index);
+    })
+})
 
-function adicionarClasse2(){
-div3.classList.add('ativo');
+
+
+function entrouMouse(){
+    let imagens = document.querySelector("#img01").src = "img/hover1.svg";
+    let texto = document.querySelector("#p01").innerHTML = "Converse com a madrinha";
 }
-function sairModal(){
-    div.classList.remove('ativo');   
-    div2.classList.remove('ativo');
-    div3.classList.remove('ativo');
+function saiuMouse(){
+    let imagens = document.querySelector("#img01").src = "img/Asset 2.svg";
+    let texto = document.querySelector("#p01").innerHTML = " ";
 }
-modal[2].addEventListener('click', adicionarClasse2)
+function entrouMouse01(){
+    let imagens = document.querySelector("#img02").src = "img/hover2.svg";
+    let texto = document.querySelector("#p02").innerHTML = "Solicitar troca de madrinha";
+}
+function saiuMouse01(){
+    let imagens = document.querySelector("#img02").src = "img/Asset 3.svg";
+    let texto = document.querySelector("#p02").innerHTML = " ";
+}
+
+
 
 
